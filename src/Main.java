dab254ba2cf7f8b05e02f945d6a9f29c6ca88c55
Victor Briche <victor.briche63@orange.fr>
2022-07-11 09:54:33 +0200

    1st commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import diceGame.Dice;
+
 public class Main {
     public static void main(String[] args) {
-
+        Dice dice1 = new Dice();
+        dice1.lancer();
+        System.out.println(dice1.get_valeur());
     }
 }
--- src/diceGame/Dice.java
+++ src/diceGame/Dice.java
@@ -1,4 +1,28 @@
 package diceGame;
 
 public class Dice {
+    //attribut valeur du dé
+    private int value;
+
+    //constructeur
+    public Dice () {
+        this.value = 0;
+    }
+
+    //méthodes
+
+    /** Getter pour avoir la valeur du dé
+     *
+     * @return la valeur du dé
+     */
+    public int get_valeur() {
+        return this.value;
+    }
+
+    /** Setter de la valeur du dé
+     *
+     */
+    public void lancer() {
+        this.value = (int) Math.ceil(Math.random()*6);
+    }
 }
